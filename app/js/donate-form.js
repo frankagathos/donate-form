c52446559e52a26749b015fd48652701dc7558a8
frankagathos <frantzeskosagathos@gmail.con>
2018-05-29 06:33:43 +0100

    latest-features

--- app/js/donate-form.js
+++ app/js/donate-form.js
@@ -1,7 +1,143 @@
-	$( function() {
+	$( function() {	
 
-			
-			// include this for all browsers ------------------------------------------------------------------
+	var donors=42;
+	var total=0;
+	
+//---------------validation starts here -----------------------------------------------------------------------------
+
+//---------------custom method for the email-----------------------
+			$.validator.addMethod("customemail", 
+			    function(value, element) {
+			        return /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(value);
+			    }, 
+			    "Sorry, I've enabled very strict email validation"
+			);	
+//---------------end of custom method-----------------------					
+			 var validator = $( "#donate-form" ).validate( {
+				errorClass: "ui-state-error",
+				rules: {
+					name: {
+						required: true,
+						lettersonly: true,
+						 minlength: 5
+					},
+					email: {
+                        required:  {
+                                depends:function(){
+                                    $(this).val($.trim($(this).val()));
+                                    return true;
+                                }   
+                            },
+                        customemail: true
+                         },
+					phonenumber:{
+						required: true,
+						digits: true,
+						minlength: 10,
+						maxlength: 14
+					},
+					creditcardnumber:{
+				     required: true,
+     				 creditcard: true,
+     				  						
+					},
+					creditcardbehind:{
+					required: true,
+     				digits: true,
+     				minlength: 3,
+     				maxlength: 3						
+					},
+					date:{
+					      required: true                          	
+					},					
+					donation:{
+					required: true,
+     				min: 0.1						
+					}					
+				},
+				messages: {
+					name: {
+						required: "Please enter your name",
+						lettersonly: "Letters only please"
+					},
+					email: {
+						required: "Please enter your email",
+						email: "Please enter a valid email"
+					},
+					phonenumber:"Please enter your phone number",
+					creditcardnumber:"Please enter your credit card number"
+
+					
+				},
+//--------------- where the errors will be placed -------------------------------------------------------
+				errorPlacement: function( error, element ) {											
+						error.prependTo( element.parent() );					
+				},
+
+//----------------next session runs if the validation is OK -----------------------------------------
+				submitHandler: function() {				
+								
+				//we take the value of donation
+			    var donation =($('#donation').val());
+				//total donation	
+				total+=1*(donation);
+				//total donors
+				donors+=1;
+					if(total>=167){
+						var moneyleft=total-167;
+					    var barvolume = 100;
+					     
+						 $("#donate-form").hide( 1000);
+						 $("#text3").hide( 1000);
+						 $("#text4").show( 1000);
+						 setTimeout(function () {
+					        $("#progbarinner").css("background-color","green");   
+					        $("#progbarinner").css("width", barvolume +"%");
+					          $("#days").css("color","green");  				        
+					          }, 1500); //will call the function after 1.5 secs.;
+						  setTimeout(function () {
+					        $("#donate-form").show( 1000);
+					        $("#text4").fadeOut( 2000);
+					        $("#text5").show( 1000);
+					        $("#surplus").text( moneyleft);
+					          }, 5000); //will call the function after 5 secs.;
+						  $("#donors").text( donors);															
+					     }				
+					else{
+						var moneyleft=total-167;
+						var barvolume = 95;
+			                $("#donate-form").hide( 1000);
+			                
+			                setTimeout(function () {
+					                  $("#money").text( -moneyleft );
+					          }, 1500); //will call the function after 1.5 secs.;
+			                		$("#donors").text( donors);
+					                //make the width bigger depending on donation
+					                setTimeout(function () {
+					        $("#progbarinner").css("background-color","green");   
+					        $("#progbarinner").css("width", barvolume +"%");				        
+					          }, 1500); //will call the function after 1.5 secs.;
+					                setTimeout(function () {
+					        $("#progbarinner").css("background-color","#f16a44");   
+					        $("#progbarinner").css("width", "60%");				        
+					          }, 4000); //will call the function after 4 secs.;
+					                setTimeout(function () {
+					        $("#donate-form").show( 1000);
+					          }, 5000); 						
+					 }
+
+//-------------- see  variables in the console	-------------------------------------------------------					    					             
+              // console.log(donation);           
+              // console.log(total);  
+              // console.log(moneyleft);
+               //console.log(donors);
+                 
+                
+        
+                
+			}
+			} );
+//---------------- include this for all browsers ------------------------------------------------------------------
 			var matched, browser;
 
 			jQuery.uaMatch = function( ua ) {
@@ -36,136 +172,31 @@
 			}
 
 			jQuery.browser = browser;
-			//end of browser ------------------------------------------------------------------
 
+//----------------------------js for the date picker--------------------- 
+        $(function() {
+            $('.date-picker').datepicker( {
+            changeMonth: true,
+            changeYear: true,
+               showButtonPanel: true,           
+            dateFormat: 'MM / yy',
+            onClose: function(dateText, inst) { 
+                $(this).datepicker('setDate', new Date(inst.selectedYear, inst.selectedMonth, 1));
+            }
+            });
+        });
 
-			//validation starts here -----------------------------------------------------------------------------
-				
-			 var counter=0;			
-			 var validator = $( "#donate-form" ).validate( {
-				errorClass: "ui-state-error",
-				rules: {
-					name: {
-						required: true,
-						lettersonly: true,
-						 minlength: 5
-					},
-					email: {
-						required: true,
-						email: true,
-						extension: "com|net|org|uk|gr|nl|gu|tv"
-					},
-					phonenumber:{
-						required: true,
-						digits: true,
-						minlength: 10,
-						maxlength: 14
-					},
-					creditcardnumber:{
-				     required: true,
-     				 creditcard: true,
-     				  						
-					},
-					creditcardbehind:{
-					required: true,
-     				digits: true,
-     				minlength: 3,
-     				maxlength: 3						
-					},
-					date:{
-					      required: true
-                          	
-					},
-					
-					donation:{
-					required: true,
-     				min: 0.1						
-					}
-					
-				},
-				messages: {
-					name: {
-						required: "Please enter your name",
-						lettersonly: "Letters only please"
-					},
-					email: {
-						required: "Please enter your email",
-						email: "Please enter a valid email"
-					},
-					phonenumber:"Please enter your phone number",
-					creditcardnumber:"Please enter your credit card number"
-
-					
-				},
-				// where the errors will be placed -------------------------------------------------------
-				errorPlacement: function( error, element ) {
-											
-						error.prependTo( element.parent() );
-					
-				},
-
-				//next session is the animation after submission -----------------------------------------
-				submitHandler: function() {				
-				 counter++;
-				 var donors =42;
-				//take value of donation
-				var donation = $('#donation').val();
-				if (donation<=167){
-							var moneyleft = 167-donation;
-							var barvolume = 95;
-							donors =donors+1;
-							//hide the whole form 1sec duration
-			                $("#donate-form").hide( 1000);
-			                
-			                setTimeout(function () {
-					                  $("#money").replaceWith( moneyleft );
-					          }, 1500); //will call the function after 1.5 secs.;
-			                		$("#donors").replaceWith( donors);
-					                //make the width bigger depending on donation
-					                setTimeout(function () {
-					        $("#progbarinner").css("background-color","green");   
-					        $("#progbarinner").css("width", barvolume +"%");				        
-					          }, 1500); //will call the function after 1.5 secs.;
-					                setTimeout(function () {
-					        $("#progbarinner").css("background-color","#f16a44");   
-					        $("#progbarinner").css("width", "60%");				        
-					          }, 4000); //will call the function after 4 secs.;
-					                setTimeout(function () {
-					        $("#donate-form").show( 1000);
-					          }, 5000); 
-
-								}
-				else{
-						var moneyleft = donation-167;
-						var barvolume = 100;
-						var donors =donors+1;
-						 $("#donate-form").hide( 1000);
-						 $("#text3").hide( 1000);
-						 $("#text4").show( 1000);
-						 setTimeout(function () {
-					        $("#progbarinner").css("background-color","green");   
-					        $("#progbarinner").css("width", barvolume +"%");				        
-					          }, 1500); //will call the function after 1.5 secs.;
-						  setTimeout(function () {
-					        $("#donate-form").show( 1000);
-					          }, 5000); //will call the function after 5 secs.;
-						  $("#donors").replaceWith( donors);
-				}
-				//If we want to take the number of money from text
-				//var money=$('#money').text();
-				//make it an integer  
-                //money=parseInt(money);
-                
-                
-
-               
-             //console.log(moneyleft); 
-             //console.log(counter); 
-             //console.log(donation);
-             //console.log(donors);    
-                //alert("Congratulations you just made a donation!");
-        
-                //validator.destroy();
-			}
-			} );
+//------------------------------js for the donate text-------------------------------------   
+     $( function() {
+    $(".smalltext2").hover(function(){
+        $(this).css("background-color", "yellow");
+        }, function(){
+        $(this).css("background-color", "white");
+    });
+    $( ".smalltext2" ).click(function() {
+  alert( "A donation of $50 is the average donation which will help us........." );
+});
+});
 		} );
+	
+	
--- app/js/intro.js
+++ app/js/intro.js
@@ -5,45 +5,23 @@ window.onload = function() {
 		
 		setTimeout(function () {
 					                  $("#text1").fadeIn( 1000 );
-					          }, 200); //will call the function after 1.5 secs.;
+					          }, 200); 
 		setTimeout(function () {
 					                  $("#text2").fadeIn( 1000 );
-					          }, 1500); //will call the function after 1.5 secs.;
+					          }, 1500); 
 		setTimeout(function () {
 					                  
 					                  $("#progressbar").show( 1000 );
 					                  $("#donate-form").show( 1000 );
-					          }, 4000); //will call the function after 1.5 secs.;
+					          }, 4000); 
 		setTimeout(function () {
 					                  $(".topheader-arrowbox").show( 1000);
-					          }, 5000); //will call the function after 1.5 secs.;
+					          }, 5000); 
 		setTimeout(function () {
 					                  $("#text3").fadeIn( 400);
-					          }, 6000); //will call the function after 1.5 secs.;
+					          }, 6000); 
 
 
-	//js for the date picker 
-        $(function() {
-            $('.date-picker').datepicker( {
-            changeMonth: true,
-            changeYear: true,           
-            dateFormat: 'MM / yy',
-            onClose: function(dateText, inst) { 
-                $(this).datepicker('setDate', new Date(inst.selectedYear, inst.selectedMonth, 1));
-            }
-            });
-        });
 
-    //js for the donate text   
-     $( function() {
-    $(".smalltext2").hover(function(){
-        $(this).css("background-color", "yellow");
-        }, function(){
-        $(this).css("background-color", "white");
-    });
-    $( ".smalltext2" ).click(function() {
-  alert( "A donation of $50 is the average donation which will help us........." );
-});
-});
 
 };
